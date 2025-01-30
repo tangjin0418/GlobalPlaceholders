3e2ce5c49e61ff79645d2aa445e9638008a4454d
TangJin <55261514+tangjin0418@users.noreply.github.com>
2025-01-30 21:45:31 +0800

    recode

--- src/main/java/org/tjdev/custom/globalplaceholders/GlobalPlaceholders.java
+++ src/main/java/org/tjdev/custom/globalplaceholders/GlobalPlaceholders.java
@@ -1,14 +1,18 @@
 package org.tjdev.custom.globalplaceholders;
 
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.tjdev.util.colorful.api.defaults.MiniMessage;
 import org.tjdev.util.tjpluginutil.ConfigUtil;
 import org.tjdev.util.tjpluginutil.config.file.YamlConfig;
 import org.tjdev.util.tjpluginutil.spigot.object.TJPlugin;
 import org.tjdev.util.tjpluginutil.spigot.plugin.papi.PAPI;
+import org.tjdev.util.tjpluginutil.text.Replacer;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 import static org.tjdev.util.tjpluginutil.IConstant.DEFAULT_COLOR_API;
@@ -20,7 +24,7 @@ public final class GlobalPlaceholders extends TJPlugin {
     public void enable() {
         DEFAULT_COLOR_API = new MiniMessage();
 
-        var map = new HashMap<String, String>();
+        var map = new LinkedHashMap<String, String>();
 
         var config = ConfigUtil.prepareResult(getDataFolder(), "config");
         var configData = YamlConfig.loadConfiguration(config.getValue());
@@ -30,20 +34,29 @@ public final class GlobalPlaceholders extends TJPlugin {
                     "player",
                     "<gray>[<white>{vault_prefix}</white>]</gray> <white>%player_displayname%</white>"
             );
-            configData.set("color.warn", "<#FCCF31>");
+            configData.set("warn", "<#FCCF31>");
         }
+        configData.options().setHeader(List.of("Process order of variables is from top to bottom."));
 
         ConfigUtil.save(configData, config.getValue());
 
-        for (String key : configData.getKeys(true))
-            if (configData.isString(key)) map.put(key, configData.getString(key));
+        for (String key : configData.getKeys(true)) {
+            if (!configData.isString(key)) continue;
+            map.put(key, configData.getString(key));
+        }
 
         papi = new PAPI() {
             @Override
+            public String request(OfflinePlayer p, String s) {
+                var rep = new Replacer(s, "[", "]", false);
+                for (Map.Entry<String, String> stringStringEntry : map.entrySet())
+                    rep.replace(stringStringEntry.getKey(), stringStringEntry.getValue());
+                return DEFAULT_COLOR_API.color(PAPI.setBracket(p, PAPI.set(p, rep.toString())));
+            }
+
+            @Override
             public String request(Player p, String s) {
-                var str = map.get(s);
-                if (str == null) return DEFAULT_COLOR_API.color("<red><white>%s</white> not exist.</red>".formatted(s));
-                return DEFAULT_COLOR_API.color(escapeLegacy(PAPI.setBracket(p, str)));
+                return request((OfflinePlayer) p, s);
             }
         };
         papi.register();
