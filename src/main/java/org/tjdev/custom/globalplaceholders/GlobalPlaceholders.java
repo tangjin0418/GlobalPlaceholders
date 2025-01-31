cbbac1c0b60f3061ba7503d9c7c52134c0d9b82a
TangJin <55261514+tangjin0418@users.noreply.github.com>
2025-01-31 09:02:19 +0800

    more

--- src/main/java/org/tjdev/custom/globalplaceholders/GlobalPlaceholders.java
+++ src/main/java/org/tjdev/custom/globalplaceholders/GlobalPlaceholders.java
@@ -1,11 +1,14 @@
 package org.tjdev.custom.globalplaceholders;
 
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.tjdev.util.colorful.api.defaults.MiniMessage;
 import org.tjdev.util.tjpluginutil.ConfigUtil;
 import org.tjdev.util.tjpluginutil.config.file.YamlConfig;
+import org.tjdev.util.tjpluginutil.spigot.locale.LangDownload;
 import org.tjdev.util.tjpluginutil.spigot.object.TJPlugin;
 import org.tjdev.util.tjpluginutil.spigot.plugin.papi.PAPI;
 import org.tjdev.util.tjpluginutil.text.Replacer;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
+import static org.tjdev.util.tjpluginutil.ConfigUtil.get;
 import static org.tjdev.util.tjpluginutil.IConstant.DEFAULT_COLOR_API;
 
 public final class GlobalPlaceholders extends TJPlugin {
@@ -29,20 +33,70 @@ public final class GlobalPlaceholders extends TJPlugin {
         var config = ConfigUtil.prepareResult(getDataFolder(), "config");
         var configData = YamlConfig.loadConfiguration(config.getValue());
 
-        if (!config.getKey()) {
-            configData.set(
+        var lang = get(configData, "lang", "en_us");
+        configData.setComments(
+                "lang",
+                List.of("If you updated this, please delete the `entity.yml` and `item.yml` in the plugin directory for re-downloading.")
+        );
+
+        var replacement = configData.getConfigurationSection("replacement");
+        if (replacement == null) {
+            replacement = configData.createSection("replacement");
+            replacement.set(
                     "player",
-                    "<gray>[<white>{vault_prefix}</white>]</gray> <white>%player_displayname%</white>"
+                    "<gray>[<white>{vault_prefix}</white>]</gray> [wonderful-color]%player_displayname%"
             );
-            configData.set("warn", "<#FCCF31>");
+            replacement.set("warn", "<#FCCF31>");
+            replacement.set("wonderful-color", "<#32CCBC>");
         }
-        configData.options().setHeader(List.of("Process order of variables is from top to bottom."));
+        configData.setComments(
+                "replacement",
+                List.of(
+                        "Process order of variables is from top to bottom.",
+                        "For example, parse `%globalplaceholders_[player]%` with `Alex_TangJin_TW (prefix ADMIN)` will be the following:",
+                        "  1. Replace [player] => `[player]` -> `<gray>[<white>{vault_prefix}</white>]</gray> [wonderful-color]%player_displayname%`",
+                        "  2. Replace [wonder-color] => `<gray>[<white>{vault_prefix}</white>]</gray> [wonderful-color]%player_displayname%` -> `<gray>[<white>{vault_prefix}</white>]</gray> <#32CCBC>%player_displayname%`",
+                        "  3. Apply placeholders => `<gray>[<white>{vault_prefix}</white>]</gray> <#32CCBC>%player_displayname%` -> `<gray>[<white>ADMIN</white>]</gray> <#32CCBC>Alex_TangJin_TW`",
+                        "  4. Color text with MiniMessage..."
+                )
+        );
 
         ConfigUtil.save(configData, config.getValue());
 
-        for (String key : configData.getKeys(true)) {
-            if (!configData.isString(key)) continue;
-            map.put(key, configData.getString(key));
+        LangDownload.init(lang, () -> {
+            var entity = ConfigUtil.prepare(getDataFolder(), "entity");
+            var entityData = YamlConfig.loadConfiguration(entity);
+            for (EntityType value : EntityType.values()) {
+                if (value == EntityType.UNKNOWN) continue;
+                map.put(
+                        "entity." + value.name(),
+                        get(entityData, value.name(), LangDownload.get(value.translationKey()))
+                );
+            }
+            configData.options().setHeader(
+                    List.of(
+                            "%globalplaceholders_[entity.ZOMBIE]% -> Zombie",
+                            "You can even add custom colors or some style like `ᴢᴏᴍʙɪᴇ`!"
+                    )
+            );
+            ConfigUtil.save(entityData, entity);
+
+            var item = ConfigUtil.prepare(getDataFolder(), "item");
+            var itemData = YamlConfig.loadConfiguration(item);
+            for (Material value : Material.values())
+                map.put("item." + value.name(), get(itemData, value.name(), LangDownload.get(value.translationKey())));
+            configData.options().setHeader(List.of(
+                    "%globalplaceholders_[item.DIAMOND_ORE]% -> Diamond Ore",
+                    "Same with above."
+            ));
+            ConfigUtil.save(itemData, item);
+
+            getLogger().info("Extra translation data has been loaded.");
+        });
+
+        for (String key : replacement.getKeys(true)) {
+            if (!replacement.isString(key)) continue;
+            map.put(key, replacement.getString(key));
         }
 
         papi = new PAPI() {
